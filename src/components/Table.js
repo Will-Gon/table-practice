7f888d425847932010294549c7611e2881c1af59
Will-Gon <will.colocho@gmail.com>
2021-07-21 23:20:56 -0700

    mess around with building table

---
+++ src/components/Table.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Table = () => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>Touchdown</tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    )
+};
+
+export default Table;
